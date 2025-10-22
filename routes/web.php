20af515c657174bf85cc08c306437e3df3980e2c
Yesther Guerra <yguerra@martech.cl>
2025-10-22 01:45:49 -0300

    Deleting routes and controllers

--- routes/web.php
+++ routes/web.php
@@ -5,25 +5,21 @@ use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\DashboardController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::group(['middleware' => ['auth']], function () {
+    Route::get('/', function () {
+        return redirect()->route('dashboard');
+    });
     Route::resource('projects', ProjectController::class);
     Route::resource('tasks', TaskController::class);
     Route::patch('/tasks/{task}/complete', [TaskController::class, 'markComplete'])->name('tasks.complete');
-
-    // Route::post('tasks/{task}/files', [App\Http\Controllers\TaskFileController::class, 'store'])->name('tasks.files.store');
-    // Route::delete('tasks/{task}/files/{file}', [App\Http\Controllers\TaskFileController::class, 'destroy'])->name('tasks.files.destroy');
+    
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
-
-    Route::get('download/{filename}', [App\Http\Controllers\DownloadFileController::class, 'download'])->name('files.download');
 });
 
 
