c8a18adad464e0a62bff344602d2950a5f6c0d22
Yesther Guerra <yguerra@martech.cl>
2025-10-22 01:06:23 -0300

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // Crear 5 usuarios
+        // Crear 6 usuarios
     $user = User::factory()->create([
         'name' => 'Test User',
         'email' => 'user@correo.com',
@@ -28,19 +28,39 @@ class DatabaseSeeder extends Seeder
     $users = User::factory(5)->create();
  
     // Crear 10 proyectos asignando dueños aleatorios de los usuarios creados
-    Project::factory(3)
+    $projectsTest = Project::factory(4)
         ->for($user)
         ->create();
 
-    $projects = Project::factory(7)
+    $projects = Project::factory(6)
         ->for($users->random())
         ->create();
+        
     // Asignar miembros a proyectos aleatoriamente (belongsToMany)
     foreach ($projects as $project) {
         $project->members()->attach(
             $users->random(rand(1, 3))->pluck('id')->toArray()
         );
     }
+
+    // Asignar miembros a proyectos aleatoriamente (belongsToMany)
+    foreach ($projectsTest as $project) {
+        $project->members()->attach(
+            $users->random(rand(1, 3))->pluck('id')->toArray()
+        );
+    }
+
+    // Crear 10 tareas asignadas a proyectos y usuarios existentes
+    Task::factory(10)
+        ->make() // crear instancia sin guardar
+        ->each(function ($task) use ($projectsTest, $users) {
+            $project = $projectsTest->random();
+            $task->project_id = $project->id;
+            // elegir usuario miembro del proyecto para asignar tarea
+            $memberIds = $project->members->pluck('id')->toArray();
+            $task->user_id = $users->whereIn('id', $memberIds)->random()->id;
+            $task->save();
+        });
 
     // Crear 10 tareas asignadas a proyectos y usuarios existentes
     Task::factory(10)
